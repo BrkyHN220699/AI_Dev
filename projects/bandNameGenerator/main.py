d6af60864b3c2dd6c27ad97f6859e4119f38303c
Berkay Han <berkayhan51@gmail.com>
2025-10-29 22:12:55 +0300

    day1Done

---
+++ projects/bandNameGenerator/main.py
@@ -0,0 +1,20 @@
+
+#print() ayrı kullanılmaz.
+#input() içine doğrudan prompt yazılır.
+#input(userCity) yanlış — input fonksiyonuna değişken değil prompt string verilir.
+
+
+
+
+
+print("Question 1")
+userCity = input("-> Which city did you grow up in? ")
+print()
+print("Question 2")
+userAnimal = input("-> Which animal do you want to pet? ")
+print()
+print("Your band name would be: " + userCity + " " + userAnimal)
+
+
+
+
